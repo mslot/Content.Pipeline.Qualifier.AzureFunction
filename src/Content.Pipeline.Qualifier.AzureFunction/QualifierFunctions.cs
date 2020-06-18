d251126f7e9de27e088230a6d4c52b425850cbf9
Martin Slot <slot@sigsys.dk>
2020-06-18 19:45:27 +0200

    Initial commit

---
+++ src/Content.Pipeline.Qualifier.AzureFunction/QualifierFunctions.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Content.Pipeline.Qualifier.AzureFunction
+{
+    public class QualifierFunctions
+    {
+        [FunctionName("QualifierFunction")]
+        [return: ServiceBus("cachebuster-topic", Connection = "Servicebus:ServicebusConnectionString")]
+        public async Task<string> Run(
+        [ServiceBusTrigger("qualifier-topic","QualifierSubscription", Connection = "Servicebus:ServicebusConnectionString")]
+        string message,
+        Int32 deliveryCount,
+        DateTime enqueuedTimeUtc,
+        string messageId,
+        ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {message}");
+            log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
+            log.LogInformation($"DeliveryCount={deliveryCount}");
+            log.LogInformation($"MessageId={messageId}");
+
+            return message;
+        }
+    }
+}
---
+++ src/Content.Pipeline.Qualifier.AzureFunction/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Content.Pipeline.Qualifier.AzureFunction.Startup))]
+namespace Content.Pipeline.Qualifier.AzureFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
